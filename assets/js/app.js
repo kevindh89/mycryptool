00e5c54718fac2ea48b3f1f4ecd760fbb4d5cff8
kevindh89 <kevin@vensterworks.com>
2018-02-04 00:59:43 +0100

    Added websocket ticker for realtime rates

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,11 @@ window.$ = $;
 require('bootstrap');
 
 window.MyCryptool = {
+    tickerWebsocket: {
+        websocket: undefined,
+        subscribers: []
+    },
+
     loadTradeList(destinationElement) {
         return $.get('/trade-list', response => {
             destinationElement.html(response);
@@ -27,5 +32,37 @@ window.MyCryptool = {
         return $.get('/api/refresh-orders', response => {
             return response;
         });
+    },
+
+    openTickerWebsocket(productId) {
+        window.MyCryptool.tickerWebsocket.websocket = new WebSocket('wss://ws-feed.gdax.com');
+
+        window.MyCryptool.tickerWebsocket.websocket.onopen = () => {
+            const subscribeMessage = {
+                "type": "subscribe",
+                "product_ids": [
+                    productId
+                ],
+                "channels": [
+                    {
+                        "name": "ticker",
+                        "product_ids": [
+                            productId
+                        ]
+                    }
+                ]
+            };
+            window.MyCryptool.tickerWebsocket.websocket.send(JSON.stringify(subscribeMessage));
+        };
+
+        window.MyCryptool.tickerWebsocket.websocket.onmessage = (event) => {
+            window.MyCryptool.tickerWebsocket.subscribers.forEach(callback => {
+                callback(event);
+            });
+        };
+    },
+
+    subscribeToTickerWebsocket(callback) {
+        window.MyCryptool.tickerWebsocket.subscribers.push(callback);
     }
 };
